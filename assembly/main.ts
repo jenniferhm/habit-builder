a048710a1698f241d25fe3b0832ef4b1f797cd35
kierankay <kierankay@gmail.com>
2020-02-16 10:59:31 -0800

    added isChallengeComplete, distributeTokens, and transferFunds methods

--- assembly/main.ts
+++ assembly/main.ts
@@ -79,25 +79,127 @@ function dateDiffInDays(challengeStart: number): number {
 export function markDailyChallengeComplete(challengeId: string, userId: string): void {
   // get the challenges
   let challenges = storage.get<object>('challenges');
-  let challengeStart = challenges[challengeId]['challenge_start_date'];
-  let challengeUserId = challenges[challengeId]['user_id'];
+  let challenge = challenges[challengeId];
+  let challengeStart = challenge['challenge_start_date'];
+  let challengeUserId = challenge['user_id'];
 
   // if the current user is the challenge creator
   // then mark participation for this day and write it back to the chain
   if (userId === challengeUserId) {
     let challengeDay = dateDiffInDays(challengeStart);
-    challenges[challengeId]['day'][challengeDay]['participant'] = true;
+    challenge['day'][challengeDay.toString()]['participant'] = true;
     storage.set('challenges', challenges);
   }
 }
 
-export function distributeTokens(): string {
-  let result = storage.get<object>("challenges");
+// once a challenge is completed
+// we want to initiate a transaction between users
+// and then 
 
-  if(result) {
-    return result;
+export function isChallengeComplete(challengeId: string, minCompletionsToWin = 25, maxMissesToLose = 5) {
+  let challenges = storage.get<object>('challenges');
+  let challenge = challenges[challengeId];
+
+  let completions = 0;
+  let challengeDays = challenge['days'];
+  for (let day in challengeDays) {
+    if (day['participant']) {
+      completions++
+    }
   }
 
-  return "";
+  if (completions > minCompletionsToWin) {
+    challenge['challenge_won'] = true;
+    distributeTokens(challenge);
+  }
 }
 
+function distributeTokens(challenge: object): void {
+  let won = challenge['challenge_won'];
+  let participantId = challenge['user_id'];
+  let participantDono = challenge['user_contribution'];
+  let supporterId = challenge['supporter_id'];
+  let supporterDono = challenge['support_user_contribution'];
+  if (won) {
+    transferFunds(participantId, supporterId, supporterDono);
+  } else {
+    transferFunds(participantId, supporterId, participantDono);
+  }
+}
+
+function transferFunds(recipient: string, donor: string, amount: number) {
+  // move amount from donor to recipient
+  // implement wallet logic
+}
+
+
+/*
+
+{
+  "challenges": {
+    "1" : {
+      "day": {
+        "0": {
+          "participant": true
+        },
+        "20": {
+          "participant": true
+        }
+        ...
+      }
+    }
+  }
+}
+
+
+{
+  "user_id": 1,
+    "user_name": "i_am_user_1",
+      "past_challenges": [
+        {
+          "challenge_id": 1,
+          "challege_start_date": "2020-01-01T01:37:56",
+          "challege_end_date": "2020-01-05T01:37:56",
+          "user_id": 1,
+          "support_usesr_id": 3,
+          "challenge_amount": 10.05,
+          "task_description": "walk my dog",
+          "day": {
+            "0": {
+              "participant" : true,
+              "support": true
+            }
+          },
+          "validation_logic": "some javascript logics to plugin"
+        },
+        {
+          "challenge_id": 3,
+          "challege_start_date": "2020-01-05T01:37:56",
+          "challege_end_date": "2020-01-10T01:37:56",
+          "user_id": 1,
+          "support_usesr_id": 6,
+          "challenge_amount": 5.05,
+          "task_description": "walk my cat",
+          "days_out_of_30": 30,
+          "validation_logic": "some javascript logics to plugin",
+          "is_fail": false,
+          "is_success": true
+        }
+      ],
+        "current_challenge": {
+    "challenge_id": 6,
+      "challege_start_date": "2020-01-01T01:37:56",
+        "challege_end_date": "2020-01-05T01:37:56",
+          "user_id": 1,
+            "support_usesr_id": 3,
+              "challenge_amount": 10.05,
+                "task_description": "walk my dog",
+                  "days_out_of_30": 10,
+                    "validation_logic": "some javascript logics to plugin",
+                      "is_fail": false,
+                        "is_success": false
+  },
+  "wallet_balance": 1000.12
+}
+
+*/
